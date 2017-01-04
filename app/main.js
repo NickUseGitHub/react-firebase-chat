bbce5653cf48b12492ec0227359e978aca1918df
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-04 16:58:19 +0700

    create simple route

--- app/main.js
+++ app/main.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
 
+//init routes
+import routes from './routes'
+
+//call appReducers
 import appReducers from './app-reducers'
 const store = createStore(appReducers)
 
 render(
   <Provider store={store}>
-    <h1>Sub, world!</h1>
+    <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
 )
