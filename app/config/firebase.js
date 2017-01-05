13f5be40c34fd626b14d833a270d76e1df7da56b
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 15:57:23 +0700

    create firebase config

---
+++ app/config/firebase.js
@@ -0,0 +1,7 @@
+export default {
+    apiKey: 'AIzaSyB1j2mm2zGqZMRzTGJzoiitUQ_mMDkpt8M',
+    // authDomain: 'react-chat-5f0cd.firebaseapp.com',
+    databaseURL: 'https://react-chat-5f0cd.firebaseio.com',
+    storageBucket: 'react-chat-5f0cd.appspot.com',
+    // messagingSenderId: '252610380534'
+}
--- app/lib/firebase/app.js
+++ app/lib/firebase/app.js
@@ -1,10 +1,5 @@
 import app from 'firebase/app'
 import database from 'firebase/database'
+import firebaseConf from '../../config/firebase'
 
-export default app.initializeApp({
-    apiKey: 'AIzaSyB1j2mm2zGqZMRzTGJzoiitUQ_mMDkpt8M',
-    // authDomain: 'react-chat-5f0cd.firebaseapp.com',
-    databaseURL: 'https://react-chat-5f0cd.firebaseio.com',
-    storageBucket: 'react-chat-5f0cd.appspot.com',
-    // messagingSenderId: '252610380534'
-})
+export default app.initializeApp(firebaseConf)
