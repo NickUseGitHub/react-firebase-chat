b1defe670606a08cfce7f519fc2f7dfd34a807f1
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 13:53:07 +0700

    fix bug submit

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    onInputchange = (type) => (e) => {
+        const value = e.target.value
+        this.setState({
+            [type]: value
+        })
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h2>Login</h2>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="password" />
+                <input type="text" placeholder="Username" onChange={this.onInputchange('username')} value={this.state.username} />
+                <input type="password" placeholder="Password"  onChange={this.onInputchange('password')} value={this.state.password} />
+                <button>Submit</button>
             </form>
         )
     }
