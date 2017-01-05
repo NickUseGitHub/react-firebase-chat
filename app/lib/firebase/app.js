f04c735677e266204fdf65b278730621ef868177
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 15:21:59 +0700

    create firebase

---
+++ app/lib/firebase/app.js
@@ -0,0 +1,11 @@
+import { app, database } from 'firebase'
+
+database()
+
+export default app.initializeApp({
+    apiKey: 'AIzaSyB1j2mm2zGqZMRzTGJzoiitUQ_mMDkpt8M',
+    // authDomain: 'react-chat-5f0cd.firebaseapp.com',
+    databaseURL: 'https://react-chat-5f0cd.firebaseio.com',
+    storageBucket: 'react-chat-5f0cd.appspot.com',
+    // messagingSenderId: '252610380534'
+})
