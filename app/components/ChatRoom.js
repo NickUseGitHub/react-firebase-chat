f35d37df56524f02ba59833fdc9a8047197c3162
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 18:07:53 +0700

    apply firebase on messageReducer

--- app/components/ChatRoom.js
+++ app/components/ChatRoom.js
@@ -6,18 +6,6 @@ import Messages from './chatrooms/Messages'
 import TextBox from './chatrooms/TextBox'
 
 export default class ChatRoom extends Component {
-
-    db = null
-
-    componentDidMount() {
-        this.db = initDb('/todos')
-        this.addTodo({name: "Hey jing di"})
-    }
-
-    addTodo(item) {
-        this.db.add(item)
-    }
-
     render() {
         return (
             <div className="col-xs-8 chatroom">
--- app/reducers/messages.js
+++ app/reducers/messages.js
@@ -1,3 +1,18 @@
+import { initDb } from '../lib/database/db'
+
+var db = null
+
 export default function(messages = [], action) {
-    return messages
+    switch(action.type) {
+        case 'INIT_ROOM': 
+            db = initDb(`/messages/${action.roomName}`)
+            return messages
+        case 'ADD_MESSAGE': 
+            db.add(action.message)
+            db.once('value').then(snapshot => {
+                messages = snapshot.val()
+            })
+            return message 
+        default: return messages
+    }
 }
