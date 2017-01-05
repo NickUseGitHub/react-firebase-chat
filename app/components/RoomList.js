f832f0eff376ffaf964f4721ab6f71f9346a4b5f
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 18:58:54 +0700

    call selectRoom ActionCreator

--- app/components/RoomList.js
+++ app/components/RoomList.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { initRoom } from '../actions/chatroom'
 import { Link } from 'react-router'
 
 class RoomList extends Component {
+
+    selectRoom = (room) => () => {
+        const { initRoom } = this.props
+        initRoom(room)
+    }
+
     render() {
         const { rooms } = this.props
 
         return (
             <div className="col-xs-4 room-list">
                 <ul className="room-list">
-                    {rooms.map(room => <li key={room.id} ><Link to={`/${room.name}`}>{room.name}</Link></li>)}
+                    {rooms.map(room => <li key={room.id} onClick={this.selectRoom(room)} ><Link to={`/${room.name}`}>{room.name}</Link></li>)}
                 </ul>
             </div>
         )
@@ -22,4 +29,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(RoomList)
+export default connect(mapStateToProps, { initRoom })(RoomList)
