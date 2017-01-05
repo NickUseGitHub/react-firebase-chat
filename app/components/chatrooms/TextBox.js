40f7c1e734751a6bbabd322599ce49cbc65604d7
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 11:44:11 +0700

    change textarea into inputText

--- app/components/chatrooms/TextBox.js
+++ app/components/chatrooms/TextBox.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 export default class TextBox extends Component {
+    state = {
+        message: ''
+    }
+
+    handleSubmit = () => (e) => {
+        e.preventDefault()
+        alert(this.state.message)
+    }
+
+    handleTextChange = () => (e) => {
+        let message = e.target.value
+        this.setState({message})
+    }
+
     render() {
         return (
             <div className="input-message">
-                <form>
-                    <textarea></textarea>
+                <form onSubmit={this.handleSubmit()}>
+                    <input type="text" onChange={this.handleTextChange()} value={this.state.message} />
                 </form>
             </div>
         )
