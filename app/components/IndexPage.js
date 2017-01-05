fb9b58c26a2a72073800529be6e248822f1f3f56
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-05 10:45:25 +0700

    Create RoomList Component

--- app/components/IndexPage.js
+++ app/components/IndexPage.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import is from 'is_js'
 
+//Components
+import RoomList from './RoomList'
+
 class IndexPage extends Component {
     render() {
         return (
             <div className="row">
                 <div className="jumbotron"><h1>Hello {this.props.member.name}</h1></div>
+                <RoomList />
             </div>
         )
     }
