7ef9dbdc57af69a6cc58f4998245692573e75b21
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-01-18 13:36:32 +0700

    include firebaseMiddleware to redux

--- app/main.js
+++ app/main.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router'
 import firebase from 'firebase/app'
 import 'firebase/database'
 import firebaseConf from './config/firebase'
 
-firebase.initializeApp(firebaseConf)
+//import middlewares
+import firebaseMiddleware from './middlewares/firebase'
 
 //init routes
 import routes from './routes'
 
+firebase.initializeApp(firebaseConf)
+
+//middlewares
+const middlewares = [firebaseMiddleware]
+
 //call appReducers
 import appReducers from './app-reducers'
-const store = createStore(appReducers)
+const store = createStore(appReducers, applyMiddleware(...middlewares))
 
 render(
   <Provider store={store}>
